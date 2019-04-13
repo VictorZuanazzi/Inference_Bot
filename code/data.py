c36533c3f7f406314ae4f6f166ebe3230bd5a0c9
Victor <victorzuanazzi@gmail.com>
2019-04-13 23:30:54 +0200

    load glove + unittest

--- code/data.py
+++ code/data.py
@@ -11,3 +11,37 @@ This file contains functionalities to:
 @author: Victor Zuanazzi
 """
 
+from torchnlp.word_to_vector import GloVe
+import os
+import numpy as np
+import torch
+
+def get_embeddings(words, embedding_type='840B'):
+    """get the word embeddings requested in words.
+    Embeddings of unknown words are a tensor of zeros.
+    Input:
+        words: (str or list(str) or dict{str:_}), the words of interest.
+        embedding_type (str), the Glove type of embeddings. Only GloVe is 
+            supported for now.
+    Returns:
+        dict{str: torch.tensor} returs a dictionary that maps the words to 
+            their embeddings.
+    """
+    
+    #convert the input words into a dictionary
+    if type(words) == str:
+        words = {words: None}
+    
+    #loads glove vectors into memory
+    glove_vectors = GloVe(embedding_type)
+        
+    #returns the dictiorary with the resquested words
+    return {w: glove_vectors[w] for w in words}
+
+def 
+    
+    
+    
+    
+    
+    
