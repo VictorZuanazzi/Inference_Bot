6abc08ec3642e98a019df9692ac5673c83e05ac8
Victor <victorzuanazzi@gmail.com>
2019-04-14 11:38:35 +0200

    Inference classifier

--- code/model.py
+++ code/model.py
@@ -7,9 +7,52 @@ The following models are implemented:
     Uni LSTM
     Bi LSTM
     Bi LSTM with max pooling
+    classifier
 
 The functionality to load word embeddings.
 
 @author: Victor Zuanazzi
 """
+import torch
+import torch.nn as nn
 
+class InferClassifier(nn.Module):
+    
+    def __init__(self, input_dim, n_classes):
+        """initializes a 2 layer MLP for classification.
+        There are no non-linearities in the original code, Katia instructed us 
+        to use tanh instead"""
+        
+        super(InferClassifier, self).__init__()
+        
+        #dimensionalities
+        self.input_dim = input_dim
+        self.n_classes = n_classes
+        self.hidden_dim = 512
+        
+        #creates a MLP
+        self.classifier = nn.Sequential(
+                nn.Linear(self.input_dim, self.hidden_dim),
+                nn.Tanh(), #not present in the original code.
+                nn.Linear(self.hidden_dim, self.n_classes))
+        
+    def forward(self, x):
+        """forward pass of the classifier
+        I am not sure it is necessary to make this explicit."""
+        
+        return self.classifier(x)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
