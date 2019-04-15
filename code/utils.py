07ea011768eb0d17463819120309d05a38a8ec3c
Victor <victorzuanazzi@gmail.com>
2019-04-15 10:21:12 +0200

    accuracy calculation

--- code/utils.py
+++ code/utils.py
@@ -4,6 +4,7 @@ Created on Sun Apr 14 10:56:54 2019
 
 @author: Victor Zuanazzi
 """
+import torch
 
 def print_flags(FLAGS):
   """
@@ -11,3 +12,23 @@ def print_flags(FLAGS):
   """
   for key, value in vars(FLAGS).items():
     print(key + ' : ' + str(value))
+    
+    
+def accuracy(predictions, targets):
+  """
+  Computes the prediction accuracy, i.e. the average of correct predictions
+  of the network.
+  Args:
+    predictions: 2D one hot econded vector[batch_size, n_classes]
+    targets: 2D vector with the class indices  [batch_size].
+  Returns:
+    accuracy: scalar float, the accuracy of predictions,
+              i.e. the average correct predictions over the whole batch
+
+  """
+  #calculates the mean accuracy over all predictions:
+  accuracy = (predictions.argmax(dim=1) == targets).type(torch.float).mean()
+  #accuracy = accuracy/len(targets)
+
+  return accuracy.item() #returns the number instead of the tensor.
+
