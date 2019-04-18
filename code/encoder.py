16276fdd558014cfa0d5b42f0f4e27ba374be6da
Victor <victorzuanazzi@gmail.com>
2019-04-18 19:22:08 +0200

    batch to seq and seq to batch to save the day :tada:

--- code/encoder.py
+++ code/encoder.py
@@ -41,17 +41,85 @@ class MeanEncoder(nn.Module):
                         x_len.view(-1, 1).to(torch.float))
         
         return out
-    
-    def build_vocab(self):
-        return get_embeddings()
         
     
-def batch_to_sequence():
-    pass
-
-def sequence_to_batch():
-    pass
+def batch_to_sequence(x, len_x, batch_first):
+    """helpful function to do the pack padding shit
+    returns the pack_padded sequence, whatever that is.
+    The data does NOT have to be sorted by sentence lenght, we do that for you!
+    Input:
+        x: (torch.tensor[max_len, batch, embedding_dim]) tensor containing the  
+            padded data. It expects the embeddings of the words in the sequence 
+            they happen in the sentence.  If batch_first == True, then the 
+            max_len and batch dimensions are transposed.
+        len_x: (torch.tensor[batch]) a tensor containing the length of each 
+            sentence in x.
+        batch_first: (bool), indicates whether batch or sentence lenght are 
+            indexed in the first dimension of the tensor.
+    Output:
+        x: (torch pack padded sequence) a the pad packed sequence containing 
+            the data. (The documentation is horrible, I don't know what a 
+            pack padded sequence really is.)
+        idx: (torch.tensor[batch]), the indexes used to sort x, this index in 
+            necessary in sequence_to_batch.
+        len_x: (torch.tensor[batch]) the sorted lenghs, also needed for 
+            sequence_to_batch."""
     
+    #sort data because pack_padded is too stupid to do it itself
+    #you can answer this after figuring the sorting out:
+    #https://stackoverflow.com/questions/49203019/how-to-use-pack-padded-sequence-with-multiple-variable-length-input-with-the-sam
+    #helpful
+    len_x, idx = len_x.sort(0, descending=True)
+    x = x[:,idx]
+          
+    #remove paddings before feeding it to the LSTM
+    x = torch.nn.utils.rnn.pack_padded_sequence(x, 
+                                                len_x, 
+                                                batch_first = batch_first)
+    
+    return x, len_x, idx
+
+def sequence_to_batch(x, len_x, idx, output_size, batch_first, all_hidden = False):
+    """helpful function for the pad packed shit.
+    Input:
+        x: (packed pad sequence) the ouptut of lstm  or pack_padded_sequence().
+        len_x (torch.tensor[batch]), the sorted leghths that come out of 
+            batch_to_sequence().
+        idx: (torch.tenssor[batch]), the indexes used to sort len_x
+        output_size: (int), the expected dimension of the output embeddings.
+        batch_first: (bool), indicates whether batch or sentence lenght are 
+            indexed in the first dimension of the tensor.
+        all_hidden: (bool), if False returs the last relevant hidden state - it 
+            ignores the hidden states produced by the padding. If True, returs
+            all hidden states.
+    Output:
+        x: (torch.tensor[batch, embedding_dim]) tensor containing the  
+            padded data.           
+    """
+    
+    #re-introduce the paddings
+    #doc pack_padded_sequence:
+    #https://pytorch.org/docs/master/nn.html#torch.nn.utils.rnn.pack_padded_sequence
+    x, _ = torch.nn.utils.rnn.pad_packed_sequence(x, 
+                                                  batch_first = batch_first)
+    if all_hidden:
+        return x
+    
+    #get the indexes of the last token (where the lstm should stop)
+    longest_sentence = max(len_x)
+    #subtracsts -1 to see what happens
+    last_word = [i*longest_sentence + len_x[i] for i in range(len(len_x))]
+    
+    #get the relevant hidden states
+    x = x.view(-1, output_size)
+    x = x[last_word,:]
+
+    #unsort the batch!
+    _, idx = idx.sort(0, descending=False)
+    x = x[idx, :]
+    
+    return x
+
 class UniLSTM(nn.Module):
     
     def __init__(self, embedding_dim=300, hidden_dim=2048, batch_size=64):
@@ -76,53 +144,14 @@ class UniLSTM(nn.Module):
         
     def forward(self, x, len_x):
         
-        
-        #sort data because pack_padded is too stupid to do it itself
-        #you can answer this after figuring the sorting out:
-        #https://stackoverflow.com/questions/49203019/how-to-use-pack-padded-sequence-with-multiple-variable-length-input-with-the-sam
-        #helpful
-
-        len_x, idx = len_x.sort(0, descending=True)
-        x = x[:,idx]
-              
-        #remove paddings before feeding it to the LSTM
-        x = torch.nn.utils.rnn.pack_padded_sequence(x, 
-                                                    len_x, 
-                                                    batch_first = self.batch_first)
+        #convert batch into a packed_pad sequence
+        x, len_x, idx = batch_to_sequence(x, len_x, self.batch_first)
         
         #run LSTM, 
         x, (_, _) = self.uni_lstm(x)
-        
-        #re-introduce the paddings
-        #doc pack_padded_sequence:
-        #https://pytorch.org/docs/master/nn.html#torch.nn.utils.rnn.pack_padded_sequence
-        x, _ = torch.nn.utils.rnn.pad_packed_sequence(x, 
-                                                      batch_first = self.batch_first)
-        
-        
-        #get the indexes of the last token (where the lstm should stop)
-        longest_sentence = max(len_x)
-        last_word = [i*longest_sentence + len_x[i] for i in range(len(len_x))]
-        
-        #get the relevant hidden states
-        x = x.view(-1, self.hidden_dim)
-        x = x[last_word,:]
-        
-#        print(f"2 shapes x= {x.shape}")
-        #unsort the batch!
-        _, idx = idx.sort(0, descending=False)
-        x = x[idx, :]
-        
-        
-        
-        
-
-#*******************this part is probably outdated.
-#        print(f"3 shapes x= {x.shape}")
-#        #reshape the data ito the correct output dimension
-        #last_hidden = last_hidden.view(-1, self.hidden_dim)
-#        print(f"3 shape x: {x.shape}")
-#        
+                
+        #takes the pad_packed_sequence and gives you the embedding vectors
+        x = sequence_to_batch(x, len_x, idx, self.output_size, self.batch_first)        
         return x
 
 class BiLSTM(nn.Module):
@@ -152,15 +181,16 @@ class BiLSTM(nn.Module):
         
     def forward(self, x, len_x):
         
+        #convert batch into a packed_pad sequence
+        x, len_x, idx = batch_to_sequence(x, len_x, self.batch_first)
+        
         #run LSTM, 
-        #we are just interested in the last hidden state
-        _, (last_hidden, _) = self.bi_lstm(x)
+        x, (_, _) = self.bi_lstm(x)
         
-        #concatenate both directons of the hidden dimention
-        last_hidden = last_hidden.view(1, 2, -1, self.hidden_dim)
-        last_hidden = torch.cat((last_hidden[:,0], last_hidden[:,1]), dim=2).view(-1, 2*self.hidden_dim)
+        #takes the pad_packed_sequence and gives you the embedding vectors
+        x = sequence_to_batch(x, len_x, idx, self.output_size, self.batch_first)
         
-        return last_hidden
+        return x
 
 
 class MaxLSTM(nn.Module):
@@ -189,11 +219,20 @@ class MaxLSTM(nn.Module):
                                 bidirectional = True)
         
     def forward(self, x, len_x):
+        
+        #convert batch into a packed_pad sequence
+        x, len_x, idx = batch_to_sequence(x, len_x, self.batch_first)
+        
         #run LSTM, 
         #we want all hidden states
         x, (_, _) = self.bi_lstm(x)
         
+        #takes the pad_packed_sequence and gives you the embedding vectors
+        x = sequence_to_batch(x, len_x, idx, self.output_size, self.batch_first, True)
+        
         #get the max value for each dimension
+        #not perfect, but will do for now. The paddings can be retrieved in case
+        #all other hidden states have negative values
         x, _ = torch.max(x, dim=0)
         
         return x
