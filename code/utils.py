e655a742bf3faa33d52237defc44994da22eabd8
Victor <victorzuanazzi@gmail.com>
2019-04-18 17:08:09 +0200

    print model moved to utils :wrench:

--- code/utils.py
+++ code/utils.py
@@ -5,6 +5,8 @@ Created on Sun Apr 14 10:56:54 2019
 @author: Victor Zuanazzi
 """
 import torch
+import os
+import numpy as np
 
 def print_flags(FLAGS):
   """
@@ -32,3 +34,21 @@ def accuracy(predictions, targets):
 
   return accuracy.item() #returns the number instead of the tensor.
 
+def print_model_params(model):
+    """prints model archtecture and numbers of parameters. 
+    Cotersy from Karan Malhotra.
+    Input: (class nn.Module) the model"""
+    
+    total = 0
+    for name, p in model.named_parameters():
+        total += np.prod(p.shape)
+        print("{:24s} {:12s} requires_grad={}".format(name, str(list(p.shape)), p.requires_grad))
+    print("\nTotal parameters: {}\n".format(total))
+
+def check_dir(path): 
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+        
+        
+        
